7f7cfa9dac704b1ba75a92792c8b179034f5a25d
Jacob Peyron <jacob.peyron@evolvetechnology.se>
2018-12-29 11:54:26 +0100

    koch: restructure to separate polygons. Nicer algo.

--- cmd/koch/koch.go
+++ cmd/koch/koch.go
@@ -12,13 +12,55 @@ import (
 	"golang.org/x/image/colornames"
 )
 
+const (
+	iterations = 4
+)
+
 type Koch struct {
-	side float64 // side length of the largest isosceles triangle
-	imd  *imdraw.IMDraw
+	polygons []*imdraw.IMDraw // first polygon is the largest
 }
 
 func New() *Koch {
-	return &Koch{}
+	return &Koch{make([]*imdraw.IMDraw, 0)}
+}
+
+type Edge struct {
+	a, b pixel.Vec
+}
+
+type Triangle struct {
+	a, b, c pixel.Vec
+}
+
+func (t Triangle) Edges() []Edge {
+	return []Edge{
+		{t.a, t.b},
+		{t.b, t.c},
+		{t.c, t.a},
+	}
+}
+
+// Only the outermost edges
+func (t Triangle) KochEdges() []Edge {
+	return []Edge{
+		{t.a, t.b},
+		{t.b, t.c},
+	}
+}
+
+func (t Triangle) Points() []pixel.Vec {
+	return []pixel.Vec{t.a, t.b, t.c}
+}
+
+func (t Triangle) Polygon() *imdraw.IMDraw {
+	imd := imdraw.New(nil)
+
+	imd.Color = colornames.Green
+	imd.EndShape = imdraw.RoundEndShape
+
+	imd.Push(t.a, t.b, t.c)
+	imd.Polygon(0)
+	return imd
 }
 
 // the height of an isosceles triangle.
@@ -27,10 +69,8 @@ func isoHeight(side float64) float64 {
 	return math.Sqrt(side*side - hside*hside)
 }
 
-func kochSplit(dst *imdraw.IMDraw, a, b pixel.Vec, it int) {
-	if it == 0 {
-		return
-	}
+// Generate a koch sub-polygon from a line between a and b
+func KochTriangle(a, b pixel.Vec) Triangle {
 
 	bigV := b.Sub(a)                 // Full side
 	bigC := a.Add(bigV.Scaled(0.5))  // Center, full side
@@ -38,57 +78,66 @@ func kochSplit(dst *imdraw.IMDraw, a, b pixel.Vec, it int) {
 	smallN := smallV.Normal()        // Normal
 	smallN = smallN.Unit().Scaled(isoHeight(smallV.Len()))
 
-	newPtA := a.Add(smallV)
-	newPtB := bigC.Add(smallN)
-	newPtC := b.Add(smallV.Scaled(-1))
-
-	it--
-
-	kochSplit(dst, a, newPtA, it)
-	dst.Push(newPtA)
-
-	kochSplit(dst, newPtA, newPtB, it)
-	dst.Push(newPtB)
-
-	kochSplit(dst, newPtB, newPtC, it)
-	dst.Push(newPtC)
-
-	kochSplit(dst, newPtC, b, it)
+	return Triangle{
+		a.Add(smallV),
+		bigC.Add(smallN),
+		b.Add(smallV.Scaled(-1)),
+	}
 }
 
 func (k *Koch) Setup() error {
-	k.side = global.Bounds.H() / 3.0
-	hSide := k.side / 2.0
-	isoH := isoHeight(k.side)
+	// The main triangle properties
+	side := global.Bounds.H() / 3.0
+	hSide := side / 2.0
+	isoH := isoHeight(side)
 	hIsoH := isoH / 2.0
 
 	m := global.Bounds.Center()
 
-	k.imd = imdraw.New(nil)
-	imd := k.imd
+	tri := Triangle{
+		pixel.V(m.X, m.Y+hIsoH),
+		pixel.V(m.X+hSide, m.Y-hIsoH),
+		pixel.V(m.X-hSide, m.Y-hIsoH),
+	}
 
-	imd.Color = colornames.Green
-	imd.EndShape = imdraw.RoundEndShape
+	k.polygons = append(k.polygons, tri.Polygon())
 
-	a := pixel.V(m.X, m.Y+hIsoH)
-	b := pixel.V(m.X+hSide, m.Y-hIsoH)
-	c := pixel.V(m.X-hSide, m.Y-hIsoH)
+	// Initialize edges to iterate
+	it := iterations
+	set := tri.Edges()
 
-	it := 4
-	imd.Push(a)
-	kochSplit(imd, a, b, it)
+	for it > 0 {
+		next := []Edge{}
+		for len(set) > 0 {
+			// Pop first element
+			var edge Edge
+			edge, set = set[0], set[1:]
 
-	imd.Push(b)
-	kochSplit(imd, b, c, it)
+			koch := KochTriangle(edge.a, edge.b)
 
-	imd.Push(c)
-	kochSplit(imd, c, a, it)
+			// Append the two smaller lines between new tri and
+			// these edge points
+			next = append(next,
+				Edge{edge.a, koch.a},
+				Edge{koch.c, edge.b},
+			)
 
-	imd.Polygon(1)
+			// Append the outer triangle edges for this koch triangle
+			next = append(next, koch.KochEdges()...)
+
+			// TODO colors
+			k.polygons = append(k.polygons, koch.Polygon())
+		}
+
+		it--
+		set = next
+	}
 
 	return nil
 }
 
 func (k *Koch) Draw(win *pixelgl.Window) {
-	k.imd.Draw(win)
+	for _, p := range k.polygons {
+		p.Draw(win)
+	}
 }
